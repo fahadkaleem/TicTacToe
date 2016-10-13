707f46de6a43205b2da4bf6adb00c8c18f9f72ac
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2016-10-13 17:26:48 -0500

    Work in progress

---
+++ game.js
@@ -0,0 +1,19 @@
+var button = [];
+var ctx = [];
+var bDisabled = [];
+var isResult = false;
+var content = [];
+
+for (var i = 1; i < 10; i++)
+    button[i] = document.getElementById('canvas' + i);
+
+for (var i = 1; i < 10; i++)
+    ctx[i] = button[i].getContext('2d');
+
+for (var i = 1; i < 10; i++)
+    bDisabled[i] = false;
+
+function loop(x){
+
+}
+
